0d693b2c7db71e5f1881f846d853991b0b02e3dd
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-10 16:07:32 +0100

    names entered

--- app.rb
+++ app.rb
@@ -11,5 +11,11 @@ class Battle < Sinatra::Base
       erb(:players)
     end
 
+    post '/names' do
+      @player_1 = params[:player_1]
+      @player_2 = params[:player_2]
+      erb(:names)
+    end
+
     run! if app_file == $0
 end
