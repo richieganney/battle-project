91102afc4fcb406ef492f1f005e04412bd037eb0
Richie <richieganney@gmail.com>
2019-07-11 15:09:39 +0100

    tidy up

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,16 @@
 class Player 
+  
+  DEFAULT_HIT_POINTS = 100
+  attr_reader :name, :hit_points
 
-  attr_reader :name
-
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
-    @hit_points = 100
+    @hit_points = hit_points
   end
 
-  def hit_points
-    @hit_points
-  end
+#   def hit_points
+#     @hit_points
+#   end
 
   def attack
     @hit_points -= 15
